9a72d48a797b1eb92c25cc30b96a4f67d5c108f3
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-05 14:58:50 +0100

    Initial commit for project setup

---
+++ app/javascript/application.js
@@ -0,0 +1,20 @@
+// Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+// import * as bootstrap from "bootstrap"
+
+import React, { StrictMode } from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { store } from "./store/greetingsStore"
+import App from "./components/App"
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'),
+);
